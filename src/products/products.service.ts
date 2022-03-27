60516ec92d1465e0f08964893af4a983154eef42
kryptondv <kryptondevpl@gmail.com>
2022-03-27 19:48:52 +0200

    removed uuid lib

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,7 +3,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { Product } from './product.model';
-import { v4 as uuidv4 } from 'uuid';
 import { merge } from 'lodash';
 
 @Injectable()
